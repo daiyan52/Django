a632e95710ac063c522ab628784fd4adc9dbba98
Daiyan Alam <daiyanalam@Daiyans-MacBook-Air.local>
2024-01-24 03:05:33 +0530

    templating with static files

--- templateproject/testapp/views.py
+++ templateproject/testapp/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+import datetime
 # Create your views here.
 def wish(request):
-    return render(request,'wish.html')
+    date = datetime.datetime.now();
+    my_dict = {'x' : date}
+    buddies_dict ={'b1': 'Daiyan','b2':'Raju','b3':'Anish'}
+    context = {**my_dict, **buddies_dict}
+    return render(request,'html/wish.html',context=context)
