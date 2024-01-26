5824f6b0f22c2f630bc891efe09a082f1f0196dd
Daiyan Alam <daiyanalam@Daiyans-MacBook-Air.local>
2024-01-26 18:15:29 +0530

    model_view

--- modelproject/modelproject/settings.py
+++ modelproject/modelproject/settings.py
@@ -14,8 +14,8 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
+TEMPLATE_DIR = BASE_DIR/'templates'
+STATIC_DIR = BASE_DIR/'static'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'modelproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -139,7 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    STATIC_DIR
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- modelproject/testapp/views.py
+++ modelproject/testapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from testapp.models import Student
 # Create your views here.
+def Student_View(request):
+    stu_list = Student.objects.all()
+    print(stu_list.count())
+    my_dict = {'stu_list':stu_list}
+    return render(request, 'HtmlFolder/index.html',context=my_dict)
