13f1d49bfb390b03ceeb30fc373df6895cfab186
daiyan52 <rehanalamkkr@gmail.com>
2024-03-27 14:51:38 +0530

    Abstract

--- Advanced_models/Advanced_models/urls.py
+++ Advanced_models/Advanced_models/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from testapp.views import DataView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',DataView)
 ]
--- Advanced_models/testapp/views.py
+++ Advanced_models/testapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from testapp.models import Student,Teacher
 # Create your views here.
+def DataView(request):
+    stu_list = Student.objects.all()
+    tea_list = Teacher.objects.all()
+    my_dict = {'stu_list': stu_list,'tea_list': tea_list}
+    return render(request, 'index.html',my_dict)
